a47545fde08c330501d63ba865ca88e06ae66386
Russell Fisher <russell_fisher@Russells-MacBook-Pro.local>
2016-05-17 17:08:46 -0600

    updated background and header layout

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var imgArray = [];
+    var getURL = "";
     var marsDate = "";
     var earthDate = "";
     var cameraAngle = "";
@@ -29,30 +31,32 @@ $(document).ready(function() {
                     getEarthDate = data.photos[arrayIndex].earth_date;
                     getSolDate = data.photos[arrayIndex].sol;
                     $('#imgPlace').append('<img src="' + roverArray[arrayIndex].img_src + '" width="100%vw" style="float"/>');
-                    $('#photoInfo').append('<h3>This image was taken on: ' + getEarthDate + '</h3><br><h3>Which is mars mission day: ' + getSolDate + '</h3><br><h3>Using the ' + whichCamera + ' camera.</h3><br><h3>Image '+(arrayIndex+1)+' of '+roverArray.length+'.</h3>');
+                    $('#photoInfo').append('<h3>This image was taken on: ' + getEarthDate + '</h3><br><h3>Which is mars mission day: ' + getSolDate + '</h3><br><h3>Using the ' + whichCamera + ' camera.</h3><br><h3>Image ' + (arrayIndex + 1) + ' of ' + roverArray.length + '.</h3>');
                     arrayIndex += 1;
                     lastAngle = cameraAngle;
                 }
             }).fail(function(data) {
-                $('#imgPlace').append('<img src="SunInterruption.png"/>');
+                $('#imgPlace').append("<br><br><br><br><br><br><br><h3>Sorry, there's no images for<br> this camera angle or this day.<br> This could be due to lack of data<br> or the Sun being between Earth and Mars<br> on the day you queried</h3>");
+                $('#photoInfo').append("<br><br><br><br><br><br><br><h3>Sorry, there's no images for<br> this camera angle or this day.<br> This could be due to lack of data<br> or the Sun being between Earth and Mars<br> on the day you queried</h3>");
             });
-        } else if (marsDate === "") {
+        } else {
             $.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + earthDate + "&camera=" + cameraAngle + "&api_key=t5rTsXuOkLjqX0VKm3ZDXDF0pUghwSUtTB5Azlgf").done(function(data) {
                 console.log(data);
                 roverArray = (data.photos);
                 if (arrayIndex >= roverArray.length) {
-                  $('#imgPlace').append("<br><br><br><br><br><br><br><h3>Sorry, that's all the images for<br> this camera angle on this day.<br> Try a different angle or change the date.</h3>");
-                  $('#photoInfo').append("<br><br><br><br><br><br><br><h3>Sorry, that's all the images for<br> this camera angle on this day.<br> Try a different angle or change the date.</h3>");
+                    $('#imgPlace').append("<br><br><br><br><br><br><br><h3>Sorry, that's all the images for<br> this camera angle on this day.<br> Try a different angle or change the date.</h3>");
+                    $('#photoInfo').append("<br><br><br><br><br><br><br><h3>Sorry, that's all the images for<br> this camera angle on this day.<br> Try a different angle or change the date.</h3>");
                 } else {
                     getEarthDate = data.photos[arrayIndex].earth_date;
                     getSolDate = data.photos[arrayIndex].sol;
                     $('#imgPlace').append('<img src="' + roverArray[arrayIndex].img_src + '" width="100%vw"/>');
-                    $('#photoInfo').append('<h3>This image was taken on: ' + getEarthDate + '</h3><br><h3>Which is mars mission day: ' + getSolDate + '</h3><br><h3>Using the ' + whichCamera + ' camera.</h3><br><h3>Image '+(arrayIndex+1)+' of '+roverArray.length+'.</h3>');
+                    $('#photoInfo').append('<h3>This image was taken on: ' + getEarthDate + '</h3><br><h3>Which is mars mission day: ' + getSolDate + '</h3><br><h3>Using the ' + whichCamera + ' camera.</h3><br><h3>Image ' + (arrayIndex + 1) + ' of ' + roverArray.length + '.</h3>');
                     arrayIndex += 1;
                     lastAngle = cameraAngle;
                 }
             }).fail(function(data) {
-                $('#imgPlace').append('<img src="SunInterruption.png"/>');
+              $('#imgPlace').append("<br><br><br><br><br><br><br><h3>Sorry, there's no images for<br> this camera angle or this day.<br> This could be due to lack of data<br> or the Sun being between Earth and Mars<br> on the day you queried</h3>");
+              $('#photoInfo').append("<br><br><br><br><br><br><br><h3>Sorry, there's no images for<br> this camera angle or this day.<br> This could be due to lack of data<br> or the Sun being between Earth and Mars<br> on the day you queried</h3>");
             });
         }
     });
